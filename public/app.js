3e05764cf349b87d81aec9ed507f50998cc37087
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-04-26 18:43:47 -0400

    odds still disappear on button click

--- public/app.js
+++ public/app.js
@@ -1,44 +1,190 @@
 
-
-
-$.getJSON("/odds", function(data) {
-
-    $(".noarticles").addClass("hidden");
-
-    $(".alert").append("<h1>Super Bowl Odds</h1>");
-
- for (var i = 0; i< data.length; i ++) {
-     $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>"); 
-
- }
-   
-})
+$(document).ready(function () {
 
 
 
-$("#bottomLink").click(function () {
+
+
+
+
+    // $.getJSON("/odds", function(data) {
+
+    //     $(".noarticles").addClass("hidden");
+
+    //     $(".alert").append("<h1>Super Bowl Odds</h1>");
+
+    //  for (var i = 0; i< data.length; i ++) {
+    //      $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>"); 
+
+    //  }
+
+    // })
+
+
+
+    $("#bottomLink").click(function () {
+        $.ajax({
+            method: "GET",
+            url: "/scrape"
+        }).then(function (data) {
+            var team = data[0].team;
+            var odds = data[0].odds;
+            console.log("team", team)
+            console.log("odds", odds)
+            $.ajax({
+                method: "GET",
+                url: "/odds"
+            }).then(function (data) {
+                console.log("here is the data"+ data);
+                $(".noarticles").addClass("hidden");
+
+                $(".alert").append("<h1>Super Bowl Odds</h1>");
+                $(".alert").html("");
+
+                for (var i = 0; i < data.length; i++) {
+                    $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Save this!</button><br/><br/><br/>");
+
+                }
+
+            })
+        })
+        console.log("I can hear you")
+    })
+
+
+
+    $("#what").click(function () {
+        $.ajax({
+
+         
+
+            method: "GET",
+            url: "/scrape"
+        }).then(function (data) {
+            console.log("Can you hear me? ")
+            var team = data[0].team;
+            var odds = data[0].odds;
+            console.log("team", team)
+            console.log("odds", odds)
+            $.ajax({
+                method: "GET",
+                url: "/odds"
+            }).then(function (data) {
+                console.log("here is the data" + data);
+                $(".noarticles").addClass("hidden");
+
+                $(".alert").append("<h1>Super Bowl Odds</h1>");
+                $(".alert").html("");
+
+                for (var i = 0; i < data.length; i++) {
+                    $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Save this!</button><br/><br/><br/>");
+
+                }
+
+            })
+        })
+       
+        console.log("I can hear you")
+      
+        
+    })
+
+
+    $(document).on("click","p",function(){
+
+        $("notes").empty();
+        var thisId = $(this).attr("data-id");
+        console.log("thisID" + thisId);
+
+        $.ajax({
+            method: "GET",
+            url: "/odds/" +thisId
+        })
+        .then(function(data){
+            console.log(data);
+            
+
+            
+            
+
+
+        })
+
+
+    })
+
   
-    $.getJSON("/scrape",function (data){
-        console.log(data);
-    }).done(function() {
 
-console.log("itsworking");
 
-$.getJSON("/odds", function (data) {
 
-    $(".noarticles").addClass("hidden");
+    //   console.log("help")
+    //     $.getJSON("/scrape",function (data){
+    //         console.log(data);
+    //     }).done(function() {
 
-    $(".alert").append("<h1>Super Bowl Odds</h1>");
+    // console.log("itsworking");
 
-    for (var i = 0; i < data.length; i++) {
-        $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>");
+    // $.getJSON("/odds", function (data) {
 
-    }
+    //     $(".noarticles").addClass("hidden");
+
+    //     $(".alert").append("<h1>Super Bowl Odds</h1>");
+
+    //     for (var i = 0; i < data.length; i++) {
+    //         $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>");
+
+    //     }
+
+    // })
+
+
+    // })
+
 
 })
+// });
+
+
+//This was my original code. 
+
+// $.getJSON("/odds", function(data) {
+
+//     $(".noarticles").addClass("hidden");
+
+//     $(".alert").append("<h1>Super Bowl Odds</h1>");
+
+//  for (var i = 0; i< data.length; i ++) {
+//      $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>"); 
+
+//  }
+   
+// })
+
+
+
+// $("#bottomLink").click(function () {
+  
+//     $.getJSON("/scrape",function (data){
+//         console.log(data);
+//     }).done(function() {
+
+// console.log("itsworking");
+
+// $.getJSON("/odds", function (data) {
+
+//     $(".noarticles").addClass("hidden");
+
+//     $(".alert").append("<h1>Super Bowl Odds</h1>");
+
+//     for (var i = 0; i < data.length; i++) {
+//         $(".alert").append("<p data-id='" + data[i]._id + "'>" + data[i].team + "<br />" + data[i].odds + "</p>" + "<button type ='button' data-id ='" + data[i]._id + "'>Make a Note</button><br/><br/><br/>");
+
+//     }
+
+// })
 
     
-})
+// })
 
 
-})
+// })
--- server.js
+++ server.js
@@ -21,8 +21,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //make a "public" a static folder
 
+
+
+
+
 //this is also the HTML route for the file index.html
 app.use(express.static("public"));
+
+app.get("/notes", function (req, res) {
+    res.sendFile(__dirname + "/public/notespage.html")
+})
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+
 
 
 mongoose.connect("mongodb://localhost/mongoscraperproject", { useUnifiedTopology: true, useNewUrlParser: true });
@@ -38,7 +51,8 @@ app.get("/scrape", function (req, res) {
             if (err) throw err;
             console.log(obj.result.n + " document(s) deleted");
         });
-    })
+    });
+    var array = []
     axios.get("https://www.vegasinsider.com/nfl/odds/futures/").then(function
         (response) {
         var $ = cheerio.load(response.data);
@@ -71,9 +85,9 @@ app.get("/scrape", function (req, res) {
 
         });
 
-        var result = {};
-        for (k = 0; k < 32; k++) {
 
+        for (k = 0; k < 32; k++) {
+            var result = {};
             teamandodds.push(teamResults[k].team +
                 ": " + oddsResults[k].odds)
 
@@ -81,38 +95,42 @@ app.get("/scrape", function (req, res) {
 
             result.team = teamResults[k].team;
             result.odds = oddsResults[k].odds;
+            array.push(result)
 
-            db.odds.create(result)
-                .then(function (dbodds) {
-                    console.log(dbodds);
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
         }
+        console.log(array)
+        db.odds.create(array)
+            .then(function (dbodds) {
+                res.send(dbodds);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
     });
-   
 
-// $.getJSON("/odds", function(data) {
-//  for (var i = 0; i< data.length; i ++) {
-//      $(".alert").append("<p data-id='"+ data[i]._id + "'>" +data[i].team + "<br />" + data[i].odds + "</p>"); 
 
-//  }
-// });
+    // $.getJSON("/odds", function(data) {
+    //  for (var i = 0; i< data.length; i ++) {
+    //      $(".alert").append("<p data-id='"+ data[i]._id + "'>" +data[i].team + "<br />" + data[i].odds + "</p>"); 
+
+    //  }
+    // });
 
     //shouldnt it say this in the address bar?
 
-    res.send("completed");
-    
-   
+
+
+
 
 });
 
 app.get("/odds", function (req, res) {
-console.log("itis");
 
     db.odds.find({})
+
         .then(function (dbodds) {
+            console.log("hey", dbodds[0])
             res.json(dbodds);
 
         })
@@ -120,6 +138,8 @@ console.log("itis");
             res.json(err);
         });
 });
+
+
 
 app.get("/odds/:id", function (req, res) {
     db.odds.findOne({ _id: req.params.id })
@@ -155,3 +175,162 @@ app.post("/odds/:id", function (req, res) {
 app.listen(Port, function () {
     console.log("App running on port " + Port);
 })
+
+
+// var express = require("express");
+// var logger = require("morgan");
+
+// var mongoose = require("mongoose");
+
+// //scraping tools
+
+// var axios = require("axios");
+// var cheerio = require("cheerio");
+
+// //Requiring all models
+
+// var db = require("./models");
+
+// var Port = 3000;
+
+// var app = express();
+
+// app.use(logger("dev"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// //make a "public" a static folder
+
+// //this is also the HTML route for the file index.html
+// app.use(express.static("public"));
+
+
+// mongoose.connect("mongodb://localhost/mongoscraperproject", { useUnifiedTopology: true, useNewUrlParser: true });
+
+// var url = "mongodb://localhost/mongoscraperproject";
+
+// app.get("/scrape", function (req, res) {
+
+//     mongoose.connect(url, function (err, db) {
+//         if (err) throw err;
+
+//         db.collection("odds").remove(function (err, obj) {
+//             if (err) throw err;
+//             console.log(obj.result.n + " document(s) deleted");
+//         });
+//     })
+//     axios.get("https://www.vegasinsider.com/nfl/odds/futures/").then(function
+//         (response) {
+//         var $ = cheerio.load(response.data);
+//         var teamResults = [];
+//         var oddsResults = [];
+//         var teamandodds = [];
+
+//         $("td.font-bold").each(function (j, element) {
+
+//             var team = $(element).text();
+//             teamResults.push({
+//                 team: team
+//             });
+
+//         });
+
+//         $("td.last").each(function (i, elements) {
+//             var odds = $(elements).text();
+//             oddsResults.push({
+//                 odds: odds
+//             });
+
+//             // db.odds.create(oddsResults[i])
+//             //     .then(function (dbodds) {
+//             //         console.log(dbodds);
+//             //     })
+//             //     .catch(function (err) {
+//             //         console.log(err);
+//             //     });
+
+//         });
+
+//         var result = {};
+//         for (k = 0; k < 32; k++) {
+
+//             teamandodds.push(teamResults[k].team +
+//                 ": " + oddsResults[k].odds)
+
+//             //    console.log(teamandodds);
+
+//             result.team = teamResults[k].team;
+//             result.odds = oddsResults[k].odds;
+
+//             db.odds.create(result)
+//                 .then(function (dbodds) {
+//                     console.log(dbodds);
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                 });
+//         }
+//     });
+   
+
+// // $.getJSON("/odds", function(data) {
+// //  for (var i = 0; i< data.length; i ++) {
+// //      $(".alert").append("<p data-id='"+ data[i]._id + "'>" +data[i].team + "<br />" + data[i].odds + "</p>"); 
+
+// //  }
+// // });
+
+//     //shouldnt it say this in the address bar?
+
+//     res.send("completed");
+    
+   
+
+// });
+
+// app.get("/odds", function (req, res) {
+// console.log("itis");
+
+//     db.odds.find({})
+//         .then(function (dbodds) {
+//             res.json(dbodds);
+
+//         })
+//         .catch(function (err) {
+//             res.json(err);
+//         });
+// });
+
+// app.get("/odds/:id", function (req, res) {
+//     db.odds.findOne({ _id: req.params.id })
+
+//         .populate("note")
+//         .then(function (dbodds) {
+
+//             res.json(dbodds);
+//         })
+//         .catch(function (err) {
+
+//             res.json(err);
+//         })
+// });
+
+// app.post("/odds/:id", function (req, res) {
+
+//     db.note.create(req.body)
+//         .then(function (dbnote) {
+
+//             return db.odds.findOneAndUpdate({ _id: req.params.id }, { note: dbodds._id }, { new: true });
+//         })
+//         .then(function (dbodds) {
+//             res.json(dbodds);
+//         })
+//         .catch(function (err) {
+//             res.json(err);
+//         });
+// });
+
+
+
+// app.listen(Port, function () {
+//     console.log("App running on port " + Port);
+// })
